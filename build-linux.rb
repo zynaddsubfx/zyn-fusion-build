a6b1da52a4e78dbb5978403f6c3c81640d741d6b
fundamental <mark.d.mccurry@gmail.com>
2017-07-05 14:17:06 -0400

    Update Build Script

--- build-linux.rb
+++ build-linux.rb
@@ -1,9 +1,13 @@
 #Settings
-CurrentVersion = "3.1.2-rc3"
+CurrentVersion = "3.0.2"
 
 def cmd(x)
     puts x
-    puts `#{x}`
+    ret = system(x)
+    if(!ret)
+        puts "ERROR: '#{x}' failed"
+        exit
+    end
 end
 
 def stage(x)
@@ -12,6 +16,7 @@ end
 
 def chdir(x)
     puts "Changing directory to #{x}"
+    Dir.chdir(x)
 end
 
 def clean()
@@ -21,45 +26,64 @@ end
 
 def get_zynaddsubfx()
     stage "Getting ZynAddSubFX"
-    cmd   "git clone https://github.com/zynaddsubfx/zynaddsubfx"
+    cmd   "git clone --depth=1 https://github.com/zynaddsubfx/zynaddsubfx"
+    chdir "zynaddsubfx"
+    cmd   "git submodule update --init"
+    chdir ".."
 end
 
 def get_zest()
     stage "Getting Zest"
-    cmd   "git clone git@fundamental-code.com:mruby-zest-build"
+    #Clone the unreleased UI submodules
+    cmd   "git clone --depth=1 git@fundamental-code.com:mruby-zest-build"
+    chdir "mruby-zest-build"
+    cmd   "git submodule update --init"
+    cmd   "ruby rebuild-fcache.rb"
+    cmd   "mv testing-cache.rb src/mruby-widget-lib/mrblib/fcache.rb"
+    cmd   "make setup"
+    cmd   "make builddep"
+    chdir ".."
 end
 
 def build_zynaddsubfx(demo_mode=true)
-    mode = demo_mode?"demo":"release"
+    mode = demo_mode ? "demo" : "release"
     stage "Building ZynAddSubFX in #{mode} mode"
-    cmd   "mkdir build-zynaddsubfx"
+    cmd   "mkdir -p build-zynaddsubfx-#{mode}"
     chdir "build-zynaddsubfx-#{mode}"
-    cmd   "cmake ../zynaddsubfx/ -DGuiModule=zest -DDemoMode=#{demo_mode}"
+    cmd   "cmake ../zynaddsubfx/ -DGuiModule=zest -DDemoMode=#{demo_mode} -DCMAKE_INSTALL_PREFIX=/usr"
     cmd   "make"
     chdir ".."
 end
 
 def build_zest(demo_mode=true)
-    mode = demo_mode?"demo":"release"
+    mode = demo_mode ? "demo" : "release"
     stage "Building Zest in #{mode} mode"
     chdir "mruby-zest-build"
     cmd   "make clean"
+    ENV["VERSION"]    = CurrentVersion
+    ENV["BUILD_MODE"] = mode
+    cmd   "rm -f package/qml/*.qml"
     cmd   "ruby rebuild-fcache.rb"
     cmd   "mv testing-cache.rb src/mruby-widget-lib/mrblib/fcache.rb"
-    cmd   "BUILD_MODE=#{mode}; make"
+    cmd   "make"
     cmd   "make pack"
+    cmd   "rm package/qml/*.qml"
     chdir ".."
 end
 
 def make_package_from_repos(demo_mode=true)
-    mode = demo_mode?"demo":"release"
+    mode = demo_mode ? "demo" : "release"
     stage "Making a package in #{mode} mode"
     chdir "build-zynaddsubfx-#{mode}"
     cmd   "sudo make install"
     chdir ".."
     chdir "mruby-zest-build"
-    cmd   "sudo linux-pack.sh"
+    cmd   "pwd"
+    #ENV["ZYN_FUSION_VERSION"] = CurrentVersion
+    cmd   "sudo ./linux-pack.sh linux-64bit-#{CurrentVersion}-#{mode}"
     chdir ".."
+    cmd   "sudo mv /opt/zyn-fusion-linux-64bit-#{CurrentVersion}-#{mode}.tar.bz2 ./"
+    cmd   "sudo chown mark zyn-fusion-linux-64bit-#{CurrentVersion}-#{mode}.tar.bz2"
 end
 
 def build_demo_package()
