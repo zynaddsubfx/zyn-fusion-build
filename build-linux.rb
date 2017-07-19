c85ef3a2ba4d8d12d6b8efdf33f879d2659d8930
fundamental <mark.d.mccurry@gmail.com>
2017-07-19 16:20:13 -0400

    Update Linux Build Script

--- build-linux.rb
+++ build-linux.rb
@@ -73,15 +73,39 @@ end
 
 def make_package_from_repos(demo_mode=true)
     mode = demo_mode ? "demo" : "release"
+    zyn  = "./build-zynaddsubfx-#{mode}"
+    this_dir = `pwd`.strip
     stage "Making a package in #{mode} mode"
     chdir "build-zynaddsubfx-#{mode}"
     cmd   "sudo make install"
     chdir ".."
-    chdir "mruby-zest-build"
-    cmd   "pwd"
-    #ENV["ZYN_FUSION_VERSION"] = CurrentVersion
-    cmd   "sudo ./linux-pack.sh linux-64bit-#{CurrentVersion}-#{mode}"
-    chdir ".."
+    cmd "sudo rm -r /opt/zyn-fusion"
+    cmd "sudo mkdir /opt/zyn-fusion"
+    cmd "sudo chown mark:users /opt/zyn-fusion"
+    cmd "sudo echo 'Version #{CurrentVersion}' >> /opt/zyn-fusion/VERSION"
+    cmd "sudo echo 'Build on'      >> /opt/zyn-fusion/VERSION"
+    cmd "sudo echo `date`          >> /opt/zyn-fusion/VERSION"
+    cmd "sudo cp   -a /usr/lib/lv2/ZynAddSubFX.lv2presets     /opt/zyn-fusion/"
+    cmd "sudo cp   -a ./zynaddsubfx/instruments/banks         /opt/zyn-fusion/"
+    cmd "sudo cp      ./mruby-zest-build/package/libzest.so   /opt/zyn-fusion/"
+    cmd "sudo cp      ./mruby-zest-build/package/zest         /opt/zyn-fusion/zyn-fusion"
+    cmd "sudo cp   -a ./mruby-zest-build/package/font         /opt/zyn-fusion/"
+    cmd "sudo mkdir  /opt/zyn-fusion/qml"
+    cmd "sudo touch  /opt/zyn-fusion/qml/MainWindow.qml"
+    cmd "sudo cp   -a ./mruby-zest-build/package/schema       /opt/zyn-fusion/"
+    cmd "sudo mkdir   /opt/zyn-fusion/ZynAddSubFX.lv2"
+    cmd "sudo cp      #{zyn}/src/Plugin/ZynAddSubFX/lv2/* /opt/zyn-fusion/ZynAddSubFX.lv2/"
+    cmd "sudo cp      #{zyn}/src/Plugin/ZynAddSubFX/vst/ZynAddSubFX.so /opt/zyn-fusion/"
+    cmd "sudo cp      #{zyn}/src/zynaddsubfx /opt/zyn-fusion/"
+    cmd "sudo cp      ./mruby-zest-build/install-linux.sh /opt/zyn-fusion/"
+    cmd "sudo cp      ./mruby-zest-build/package-README.txt /opt/zyn-fusion/README.txt"
+    cmd "sudo cp      ./mruby-zest-build/zyn-fusion-ELUA.txt /opt/zyn-fusion/"
+    cmd "sudo cp      ./zynaddsubfx/COPYING /opt/zyn-fusion/COPYING.zynaddsubfx"
+    chdir "/opt/"
+    cmd "sudo rm -f zyn-fusion-linux-64bit-#{CurrentVersion}-#{mode}.tar zyn-fusion-linux-64bit-#{CurrentVersion}-#{mode}.tar.bz2"
+    cmd "sudo tar cf zyn-fusion-linux-64bit-#{CurrentVersion}-#{mode}.tar ./zyn-fusion"
+    cmd "sudo bzip2 zyn-fusion-linux-64bit-#{CurrentVersion}-#{mode}.tar"
+    chdir "#{this_dir}/"
     cmd   "sudo mv /opt/zyn-fusion-linux-64bit-#{CurrentVersion}-#{mode}.tar.bz2 ./"
     cmd   "sudo chown mark zyn-fusion-linux-64bit-#{CurrentVersion}-#{mode}.tar.bz2"
 end
@@ -106,6 +130,8 @@ def display_reminders()
     puts "TODO put various reminders here for release process"
     puts "     (notes should be in zyn's docs)"
 end
+
+cmd "sudo echo sudo"
 
 clean()
 
