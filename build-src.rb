8ec58e18a51dc3d1ef63b7f8a378654f1ba0bd7f
fundamental <mark.d.mccurry@gmail.com>
2017-07-20 16:23:23 -0400

    Add Source Tarball Build Script

---
+++ build-src.rb
@@ -0,0 +1,42 @@
+#Settings
+CurrentVersion = "3.0.2"
+
+def cmd(x)
+    puts x
+    ret = system(x)
+    if(!ret)
+        puts "ERROR: '#{x}' failed"
+        exit
+    end
+end
+
+def stage(x)
+    puts "# #{x}"
+end
+
+def chdir(x)
+    puts "Changing directory to #{x}"
+    Dir.chdir(x)
+end
+
+def clean()
+    dir = "zyn-fusion-ui-src-#{CurrentVersion}"
+    cmd "rm -rf #{dir}"
+    cmd "rm -f #{dir}.tar"
+    cmd "rm -f #{dir}.tar.bz2"
+end
+
+def get_zest()
+    dir = "zyn-fusion-ui-src-#{CurrentVersion}"
+    stage "Getting Zest"
+    #Clone the unreleased UI submodules
+    cmd   "git clone --depth=1 git@fundamental-code.com:mruby-zest-build #{dir}"
+    chdir "#{dir}"
+    cmd   "git submodule update --init"
+    cmd   "git archive-all --prefix #{dir} ../#{dir}.tar"
+    chdir ".."
+    cmd   "bzip2 #{dir}.tar"
+end
+
+clean()
+get_zest()
