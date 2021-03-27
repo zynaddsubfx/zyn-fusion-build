31a72d2b8c11ab3203f7e716ff9b757bdd2bb3c2
AnClark <clarklaw4701@qq.com>
2021-03-27 15:28:01 +0800

    Clean: Unnecessary/old build files
    Those files cleaned are old build scripts or sources.
    They are no longer required. Use Makefiles instead.

--- build-libuv.sh
+++
@@ -1,4 +0,0 @@
-cd deps/libuv-v1.9.1
-./autogen.sh
-./configure  --host=x86_64-w64-mingw32
-LD=x86_64-w64-mingw32-gcc make
--- z/build-fftw.sh
+++
@@ -1,8 +0,0 @@
-set -e
-wget http://www.fftw.org/fftw-3.3.4.tar.gz
-tar xvf fftw*tar.gz --skip-old-files  
-cd fftw*
-./configure --host=x86_64-w64-mingw32 --prefix=`pwd`/../pkg/ --with-our-malloc --disable-mpi
-make
-make install
-cd ..
